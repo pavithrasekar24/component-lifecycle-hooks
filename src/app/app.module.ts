81d9846baf9d3b60d441b2b7c0b92365c1bcc613
pavithrasekar24 <spavithracse8@gmail.com>
2022-03-18 20:20:38 +0530

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { AddProductComponent } from './add-product/add-product.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
@@ -14,6 +15,7 @@ import { AddProductComponent } from './add-product/add-product.component';
     HelloComponent,
     ProductDetailsComponent,
     AddProductComponent,
+    FooterComponent,
   ],
   bootstrap: [AppComponent],
 })
